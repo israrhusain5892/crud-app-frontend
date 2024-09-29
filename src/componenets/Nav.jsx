7a5d64397fb7155f9894988dc51f304d9d138431
israr husain <54683061+israrhusain5892@users.noreply.github.com>
2024-09-29 19:56:24 +0530

    Update Nav.jsx

--- src/componenets/Nav.jsx
+++ src/componenets/Nav.jsx
@@ -10,15 +10,13 @@ const Nav = () => {
                }}
             className="navbar navbar-expand-lg navbar-light bg-primary text-white sticky-top ">
                 <div className="container-fluid">
-                    <a className="navbar-brand text-white" href="#">Dashboard</a>
+                    <a className="navbar-brand text-white" href="#">CRM-Customer Relationship Manager</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
-                            </li>
+    
 
 
 
